3923a796f6853f0d714bd41baf3b0e792cfaf006
Irwanto <irwanto_kong@yahoo.co.id>
2020-10-26 20:37:57 +0700

    Init project

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/irwanto/jotit/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.mobileprogramming.irwanto.jotit.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.ui.cs.mobileprogramming.irwanto.jotit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
